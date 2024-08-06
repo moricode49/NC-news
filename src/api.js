abc0c7ab56a648f3327cff02f09975f4e85e57d9
moricode49 <samhurley4@hotmail.com>
2024-08-06 09:48:02 +0100

    add ticket-3-view-an-individual-article

---
+++ src/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const newsAPI = axios.create({
+	baseURL: "https://nc-news-lrsx.onrender.com/api/",
+});
+
+export const fetchAllArticles = () => {
+	return newsAPI.get("/articles").then((response) => {
+		return response.data;
+	});
+};
+
+export const fetchArticleById = (id) => {
+	return newsAPI.get(`/articles/${id}`).then((response) => {
+		return response.data;
+	});
+};
