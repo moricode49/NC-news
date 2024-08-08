1f1ede7542a15b0a5c9f1cc479fb6dbeb4d0488f
moricode49 <samhurley4@hotmail.com>
2024-08-08 11:00:06 +0100

    add sort articles

--- src/TopicCoding.jsx
+++ src/TopicCoding.jsx
@@ -1,26 +1,82 @@
 import { useEffect, useState } from "react";
-import { fetchArticles } from "./api";
+import { fetchArticles, fetchTopics } from "./api";
 import ArticleCard from "./ArticleCard";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function TopicCoding() {
 	const [topicsCoding, setTopicsCoding] = useState([]);
+	const [topics, setTopics] = useState([]);
+	const [searchParams, setSearchParams] = useSearchParams();
+	const sortByQuery = searchParams.get("sort_by");
+	const orderQuery = searchParams.get("order");
 
 	useEffect(() => {
-		fetchArticles("coding").then(({ articles }) => {
+		fetchArticles(sortByQuery, orderQuery, "coding").then(({ articles }) => {
 			setTopicsCoding(articles);
 		});
-	}, []);
+		fetchTopics().then(({ topics }) => {
+			setTopics(topics);
+		});
+	}, [sortByQuery, orderQuery]);
 
-	return topicsCoding.map((article) => {
-		return (
-			<ArticleCard
-				key={article.article_id}
-				author={article.author}
-				title={article.title}
-				topic={article.topic}
-				img_url={article.article_img_url}
-				article_id={article.article_id}
-			/>
-		);
-	});
+	const setSortBy = (sort) => {
+		const newParams = new URLSearchParams(searchParams);
+		newParams.set("sort_by", sort);
+		setSearchParams(newParams);
+	};
+
+	const setSortOrder = (direction) => {
+		const newParams = new URLSearchParams(searchParams);
+		newParams.set("order", direction);
+		setSearchParams(newParams);
+	};
+
+	return (
+		<>
+			<nav>
+				<h3>Topics: </h3>
+				{topics.map((topic) => {
+					const path = "/articles/" + topic.slug;
+					return (
+						<Link to={path} key={topic.slug}>
+							{topic.slug.toUpperCase()}
+						</Link>
+					);
+				})}
+			</nav>
+			<div id="sort-by-div">
+				Sort by:{" "}
+				<select
+					id="sort-by-select"
+					onChange={(e) => {
+						setSortBy(e.target.value);
+					}}
+				>
+					<option value="created_at">date</option>
+					<option value="comment_count">comments</option>
+					<option value="votes">votes</option>
+				</select>
+				Order:{" "}
+				<select
+					id="order-by-select"
+					onChange={(e) => setSortOrder(e.target.value)}
+				>
+					<option value="desc">desc</option>
+					<option value="asc">asc</option>
+				</select>
+			</div>
+			{topicsCoding.map((article) => {
+				return (
+					<ArticleCard
+						key={article.article_id}
+						author={article.author}
+						title={article.title}
+						topic={article.topic}
+						img_url={article.article_img_url}
+						article_id={article.article_id}
+					/>
+				);
+			})}
+		</>
+	);
 }
