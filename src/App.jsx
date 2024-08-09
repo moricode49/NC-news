ce0522cc9e3ff50a5fcf9e857ec4a36aeb770f8f
moricode49 <samhurley4@hotmail.com>
2024-08-09 14:04:06 +0100

    add ticket-9 sort articles

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import TopicCoding from "./TopicCoding";
 import TopicCooking from "./TopicCooking";
 import TopicFootball from "./TopicFootball";
 import RouteError from "./RouteError";
+import TopicProvider from "./TopicProvider";
 
 function App() {
 	const [loggedInUser, setLoggedInUser] = useState({});
@@ -27,9 +28,9 @@ function App() {
 					<Route path="/articles/:article_id" element={<Article />} />
 					<Route path="/users" element={<Users />} />
 					{/* //fetch to get routes? */}
-					<Route path="/topics/coding" element={<TopicCoding />} />
-					<Route path="/topics/football" element={<TopicFootball />} />
-					<Route path="/topics/cooking" element={<TopicCooking />} />
+					<Route path="/topics/:topic" element={<TopicProvider />} />
+					{/* <Route path="/topics/football" element={<TopicFootball />} /> */}
+					{/* <Route path="/topics/cooking" element={<TopicCooking />} /> */}
 					<Route path="*" element={<RouteError />} />
 				</Routes>
 			</UserContext.Provider>
